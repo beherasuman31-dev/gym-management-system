c9bb7cc533ad9165b82c50f57ade494ccc54898c
Suman Behera <beherasuman31@gmail.com>
2026-05-10 21:33:47 +0530

    Fixed cloud database connection

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const path = require("path");
 const bcrypt = require("bcrypt");
 
+require("dotenv").config();
+
 const app = express();
 
 app.use(cors());
@@ -20,11 +22,11 @@ app.get("/test", (req, res) => {
 
 // ================= MYSQL =================
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Behera@123",
-  database: "gym_db",
-  port: 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 db.connect((err) => {
